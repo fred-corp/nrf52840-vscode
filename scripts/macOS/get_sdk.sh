4e24b3267c17f82f6d636cdcfddf7491f6f8d7fa
fred-corp <Frederic.druppel@icloud.com>
2024-11-29 23:06:53 +0100

    commented line that has been uncommented

--- scripts/macOS/get_sdk.sh
+++ scripts/macOS/get_sdk.sh
@@ -9,12 +9,12 @@ open -a Safari "https://www.nordicsemi.com/Products/Development-software/nRF5-SD
 echo "Once downloaded, extract the SDK to the nrf-sdk folder"
 echo ""
 
-## wait for the user to press a key
+# wait for the user to press a key
 read -n 1 -s -r -p "Press any key to continue"
 
-# # Clone and install micro-ecc
+# Clone and install micro-ecc
 git clone --depth 1 https://github.com/kmackay/micro-ecc.git nrf-sdk/external/micro-ecc/micro-ecc
-replace "/usr/local/gcc-arm-none-eabi-9-2020-q2-update/bin/" with "/usr/local/bin" in "nrf-sdk/components/toolchain/gcc/Makefile.posix"
+# replace "/usr/local/gcc-arm-none-eabi-9-2020-q2-update/bin/" with "/usr/local/bin" in "nrf-sdk/components/toolchain/gcc/Makefile.posix"
 sed -i '' 's/\/usr\/local\/gcc-arm-none-eabi-9-2020-q2-update\/bin\//\/usr\/local\/bin\//g' nrf-sdk/components/toolchain/gcc/Makefile.posix
 (cd nrf-sdk/external/micro-ecc/nrf52hf_armgcc/armgcc; make;)
 
